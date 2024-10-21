e52821cf18ac715a694dffc21603e7962f3abfd5
Faria Sultana <fs1231@g.rit.edu>
2024-10-20 22:26:00 -0400

    tcpdump code reformatted

--- architecture.py
+++ architecture.py
@@ -1,5 +1,5 @@
 import capture
-import cleaner
+import cleaner2
 import statmaker
 import csvmaker
 
@@ -7,7 +7,7 @@ def main():
     print(" \n\n Welcome to Faria's version of Wireshark \n\n")
 
     capture.main()
-    cleaner.parse_packet_file('foobar.txt', 'cleaned_foobar.txt')
+    cleaner2.process_tcpdump_output('foobar.txt','cleaned_foobar.txt')
     statmaker.analyze_packets_from_file("cleaned_foobar.txt")
     csvmaker.parse_packets("cleaned_foobar.txt", "foobar_csv.csv")
 
