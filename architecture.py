2b506b8fe8e5d66d45bc506548bd216b6dce69f2
Faria Sultana <fs1231@g.rit.edu>
2024-10-28 00:57:41 -0400

    packet classifier

--- architecture.py
+++ architecture.py
@@ -7,23 +7,31 @@ def main():
     print("\n\n Welcome to Faria's version of Wireshark \n\n")
 
     while True:
-        if ask_user("Do you want to start the capture process? (y/n)"):
+        if ask_user("Do you want to start the capture process? (y/n) Q to quit") == 'y':
             capture.main()
+        elif ask_user("Do you want to start the capture process? (y/n) Q to quit") == 'q':
+            break
         else:
             continue
 
-        if ask_user("Do you want to clean the tcpdump output? (y/n)"):
+        if ask_user("Do you want to clean the tcpdump output? (y/n) Q to quit") == 'y':
             cleaner2.process_tcpdump_output('foobar.txt', 'cleaned_foobar.txt')
+        elif ask_user("Do you want to clean the tcpdump output? (y/n) Q to quit")  == 'q':
+            break
         else:
             continue
 
-        if ask_user("Do you want to see some stats? (y/n)"):
+        if ask_user("Do you want to see some stats? (y/n) Q to quit") == 'y':
             statmaker.analyze_packets_from_file("cleaned_foobar.txt")
+        elif ask_user("Do you want to see some stats? (y/n) Q to quit") == 'q':
+            break
         else:
             continue
 
-        if ask_user("Do you want to parse packets into a CSV file? (y/n)"):
+        if ask_user("Do you want to parse packets into a CSV file? (y/n) Q to quit") == 'y':
             csvmaker.parse_packets("cleaned_foobar.txt", "foobar_csv.csv")
+        elif ask_user("Do you want to parse packets into a CSV file? (y/n) Q to quit") == 'q':
+            break
         else:
             continue
         
@@ -34,8 +42,12 @@ def main():
 def ask_user(question):
     while True:
         response = input(question).strip().lower()
-        if response in ['y', 'n']:
+        if response in ['y', 'Y']:
             return response == 'y'
-        print("Invalid input. Please enter 'y' for yes or 'n' for no.")
+        elif response == 'Q':
+            return response == 'q'
+        print("Invalid input")
+        return response =='y'
+        
 
 main()
