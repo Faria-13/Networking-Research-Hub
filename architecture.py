88f0b7bf62870465ee8fa80443e9bc21dd9f39b4
Faria Sultana <fs1231@g.rit.edu>
2024-10-28 01:14:33 -0400

    refactoring main

--- architecture.py
+++ architecture.py
@@ -7,30 +7,30 @@ def main():
     print("\n\n Welcome to Faria's version of Wireshark \n\n")
 
     while True:
-        if ask_user("Do you want to start the capture process? (y/n) Q to quit") == 'y':
+        if ask_user("Do you want to start the capture process? (y/n) Q to quit ") == True:
             capture.main()
-        elif ask_user("Do you want to start the capture process? (y/n) Q to quit") == 'q':
+        elif ask_user("Do you want to start the capture process? (y/n) Q to quit ") == 'q':
             break
         else:
             continue
 
-        if ask_user("Do you want to clean the tcpdump output? (y/n) Q to quit") == 'y':
+        if ask_user("Do you want to clean the tcpdump output? (y/n) Q to quit") == True:
             cleaner2.process_tcpdump_output('foobar.txt', 'cleaned_foobar.txt')
-        elif ask_user("Do you want to clean the tcpdump output? (y/n) Q to quit")  == 'q':
+        elif ask_user("Do you want to clean the tcpdump output? (y/n) Q to quit") == 'q':
             break
         else:
             continue
 
-        if ask_user("Do you want to see some stats? (y/n) Q to quit") == 'y':
+        if ask_user("Do you want to see some stats? (y/n) Q to quit ") == True:
             statmaker.analyze_packets_from_file("cleaned_foobar.txt")
         elif ask_user("Do you want to see some stats? (y/n) Q to quit") == 'q':
             break
         else:
             continue
 
-        if ask_user("Do you want to parse packets into a CSV file? (y/n) Q to quit") == 'y':
+        if ask_user("Do you want to parse packets into a CSV file? (y/n) Q to quit ") == True:
             csvmaker.parse_packets("cleaned_foobar.txt", "foobar_csv.csv")
-        elif ask_user("Do you want to parse packets into a CSV file? (y/n) Q to quit") == 'q':
+        elif ask_user("Do you want to parse packets into a CSV file? (y/n) Q to quit ") == 'q':
             break
         else:
             continue
@@ -38,6 +38,8 @@ def main():
         # If all processes are completed, break the loop
         print("All processes completed. Exiting the program.")
         break
+
+
 
 def ask_user(question):
     while True:
@@ -48,6 +50,18 @@ def ask_user(question):
             return response == 'q'
         print("Invalid input")
         return response =='meh'
-        
+
+
+def ask_user(question):
+    while True:
+        response = input(question).strip().lower()
+        if response in ['y', 'yes']:  # Adjusted to also accept 'yes'
+            return True
+        elif response in ['n', 'no']:  # Added option to return False for 'no'
+            return False
+        elif response in ['q', 'quit']:  # Adjusted to check for 'quit'
+            return 'q'
+        print("Invalid input. Please enter 'y', 'n', or 'q'.")
+
 
 main()
