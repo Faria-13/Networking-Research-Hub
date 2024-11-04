e3e96fc413b708a8578d45056e9e650c0a2f7914
Faria Sultana <fs1231@g.rit.edu>
2024-11-04 02:35:47 -0500

    take out keyboard interrupt

--- capture.py
+++ capture.py
@@ -53,12 +53,12 @@ def capture_traffic(interface, output_file, num_of_packets, num_of_files):
                 # Start the tcpdump process and redirect stdout to the file
                 process = subprocess.Popen(capture_command, stdout=file, stderr=subprocess.PIPE)
                 stdout, stderr = process.communicate()
-                try:
-                    # Wait for the process to finish or until keyboard interrupt (Ctrl+C)
-                    process.wait()
-                except KeyboardInterrupt:
-                    print("\nCapture stopped.")
-                    process.terminate()
+                # try:
+                #     # Wait for the process to finish or until keyboard interrupt (Ctrl+C)
+                #     process.wait()
+                # except KeyboardInterrupt:
+                #     print("\nCapture stopped.")
+                #     process.terminate()
         
         print(f"Output saved to {capture_file_list}.")
     
